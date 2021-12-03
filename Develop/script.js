13729366ff658cc01b4422222930476c268aae1d
Chris T <ctijerina@live.com>
2021-12-02 23:29:36 -0600

    added the ability to save to  local storage and populating the page with storage items on load

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,54 @@ var dateToday = date.toLocaleDateString(
     day: "numeric"
 })
 
+//page load function
+var loadEvents = function() {
+    for ( i = 0; i < localStorage.length; i++) {
+        var storageSlot = localStorage.key([i])
+        $("[data-hour =" + storageSlot + "]").siblings(".description").text(JSON.parse(localStorage.getItem(storageSlot)))
+    }
+}
 
 // Display date at top of page
 $("#currentDay").text(today + " " + dateToday)
 
+// clicking on time block 
+$(".description").on("click", function(){
+    var text = $(this)
+        .text()
+        .trim();
+
+//create text input area
+    var textInput = $("<textarea>")
+        .addClass("event-entry col-10")
+        .val(text);
+    $(this).replaceWith(textInput);
+
+// auto focus new element
+    textInput.trigger("focus");
+});
+
+
+$(".saveBtn").on("click", function() {
+    // var hour = ($(this).parent().children(".hour").text().trim())
+    var hour = ($(this).parent().children(".hour").attr("data-hour"))
+    var eventText = ($(this).parent().children(".event-entry").val())
+    localStorage.setItem(hour, JSON.stringify(eventText))
+})
+
+loadEvents();
+
+// // editable field was un-focused
+// $(".event-entry").on("blur", "textarea", function () {
+//     // get current value of textarea
+//     var text = $(this).val();
+//     console.log
+// })
+
+
+
+
+// function saveEvent() {
+//     $(this).closest("div", "description")
+//     localStorage.setItem(time, JSON.stringify(text))
+// }
